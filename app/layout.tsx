d751bc08495eeef90a6fe6d63afa10b3f2130d5c
Wesley Noss <wesley.noss@virginpulse.com>
2024-11-25 18:24:54 -0500

    project structure update 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <header>
+          Trip Planner
+        </header>
         {children}
       </body>
     </html>
